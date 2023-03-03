138092a08d427b739f3ba701d60c7001d50ade15
Çağrı <doseyenc@gmail.com>
2023-03-03 17:41:43 +0300

    trending fragment add

---
+++ app/src/main/java/com/example/themovie/trending/ui/TrendingFragment.kt
@@ -0,0 +1,23 @@
+package com.example.themovie.trending.ui
+
+
+import com.example.themovie.common.view.BaseFragment
+import com.example.themovie.databinding.FragmentTrendingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TrendingFragment : BaseFragment<FragmentTrendingBinding>() {
+    override fun init() {
+        setupView()
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+
+    }
+
+    private fun setupView() {
+
+    }
+
+}
