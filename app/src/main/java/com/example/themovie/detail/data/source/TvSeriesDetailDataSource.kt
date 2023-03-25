771a7a28137725e6abe22e8136b62307a04553f5
Çağrı <doseyenc@gmail.com>
2023-03-26 01:49:06 +0300

    TvSeriesDetailRemoteDataSource add

--- app/src/main/java/com/example/themovie/detail/data/source/TvSeriesDetailDataSource.kt
+++ app/src/main/java/com/example/themovie/detail/data/source/TvSeriesDetailDataSource.kt
@@ -4,7 +4,7 @@ package com.example.themovie.detail.data.source
 import com.example.themovie.detail.data.model.TvSeriesDetailResponse
 import io.reactivex.rxjava3.core.Observable
 
-interface TvSeriesDataSource {
+interface TvSeriesDetailDataSource {
     interface Remote {
         fun getTvSeriesDetail(
             language: String,
