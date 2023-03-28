cdce04cae3816b0586da15574260ff54f6f1c0b2
Çağrı <doseyenc@gmail.com>
2023-03-29 01:35:02 +0300

    TvSeries fragment done

---
+++ app/src/main/java/com/example/themovie/detail/ui/TvSeriesDetailFragment.kt
@@ -0,0 +1,21 @@
+package com.example.themovie.detail.ui
+
+import com.example.themovie.common.view.BaseFragment
+import com.example.themovie.databinding.FragmentTvSeriesDetailBinding
+
+
+class TvSeriesDetailFragment : BaseFragment<FragmentTvSeriesDetailBinding>() {
+    override fun init() {
+        setupView()
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+
+    }
+
+    private fun setupView() {
+
+    }
+
+}
